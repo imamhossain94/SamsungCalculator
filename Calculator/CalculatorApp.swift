d8cf4f2c4580aade34193937eb239c3ea6ff59e5
imamhossain94 <imamagun94@gmail.com>
2026-03-27 17:41:13 +0600

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Md Imam Hossain on 3/27/26.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
